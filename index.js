0d9ddf7b0586a32a84abc27c5cf50d8b96525b87
Nieves <35975813+niecogi@users.noreply.github.com>
2021-07-28 18:57:59 +0200

    first auth works

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 //IMPORTS ROUTES
 const authRoute = require('./routes/auth');
 
 
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    () => console.log('connected to db!')  
+);
+//middleware
+app.use(express.json());
 
 //Route Middleware
-
 app.use('/api/user',authRoute)
-
-
 
 app.listen(3000, () => console.log('Server UP and running'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+// we need a body parser
+// and we need some time to submit to database so we need a
+//async method
+router.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password      
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+
+    }catch(err){
+        res.status(400).send("HOLA");
+    }
+   
 });
 
 /*
